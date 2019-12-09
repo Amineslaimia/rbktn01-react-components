3dd7feee6699c01681a9037cb12a4c3bc2581034
amineslaimia <medamineslaimia@gmail.com>
2019-12-09 19:12:57 +0100

    done

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li>{this.props.list}</li>
+    );
+  }
+
+}
+var GroceryList = (props) => (
+  <ul>
+    {props.lists.map(list =>
+      <GroceryListItem list={list} />
+    )}
+  </ul>
+);
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryList lists={['milk', 'bananas', 'eggs']}/> 
+  </div>
+);
+ 
+ReactDOM.render(<App/>, document.getElementById('app'));
